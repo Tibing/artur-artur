41c5ee3f0411138e030ddba39c7e943d6d69a8b8
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-10-21 20:27:21 +0300

    feat(vk): add neat icon

--- js/content_scripts/vk-content-scripts.js
+++ js/content_scripts/vk-content-scripts.js
@@ -11,6 +11,7 @@ chrome.runtime.onMessage.addListener(
 
 const dataAttr = 'data-play-to-kodi';
 const audioClassName = 'audio_row';
+const audioRawActionsClass = 'audio_row__actions';
 
 const tracks = [ ...document.querySelectorAll(`.${audioClassName}`) ];
 tracks.forEach(updateOneAudio);
@@ -35,16 +36,7 @@ observer.observe(document.body, {
 });
 
 function updateOneAudio(audio) {
-  'use strict';
   var link = null;
-
-  var audioActs = audio.querySelector('.audio_row__info');
-  //audioActs = audioActs.
-  //console.log(audioActs);
-  if (audioActs.querySelector('.downloadButton')) {
-    return;
-  }
-  // console.log("audio " + audioActs);
 
   var info = getAudioInfo(audio);
 
@@ -62,67 +54,72 @@ function updateOneAudio(audio) {
     track_addon: info.track_addon
   };
 
-  var downloadButton = document.createElement('div');
-  downloadButton.className = 'downloadButton audio_act';
-
-  downloadButton.setAttribute('style', 'display:block; margin: 17px 5px 0 5px');
-  const htmlLink = document.createElement('a');
-  htmlLink.innerText = 'Play to Kodi';
-  htmlLink.className = 'downloadButton fngfng';
-  htmlLink.setAttribute('download', stringTitle);
   audio.setAttribute(dataAttr, JSON.stringify(audiosToDownload));
 
-  htmlLink.setAttribute('href', link);
-  htmlLink.href = '#';
+  new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type === 'childList') {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+          const actions = mutation.addedNodes[ i ];
 
-  downloadButton.appendChild(htmlLink);
+          if (actions.classList && actions.classList.contains(audioRawActionsClass)) {
+            const shareIcon = document.createElement('div');
+            shareIcon.classList.add(...[ 'audio_row__action' ]);
+            shareIcon.setAttribute('download', stringTitle);
+            shareIcon.setAttribute('style', `
+              background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%3E%0A%20%20%3Cpath%20fill%3D%22%23828A99%22%20fill-rule%3D%22nonzero%22%20d%3D%22M8.557%2016.819a.9.9%200%201%200%201.084-1.438c-1.176-.886-1.841-2.099-1.841-3.38%200-1.283.665-2.496%201.841-3.382A.9.9%200%200%200%208.558%207.18C6.95%208.393%206%2010.125%206%2012.001c0%201.874.949%203.606%202.557%204.818zM12%2014a2%202%200%201%200%200-4%202%202%200%200%200%200%204zm3.443%202.819C17.05%2015.607%2018%2013.875%2018%2012s-.95-3.607-2.559-4.819a.9.9%200%201%200-1.083%201.438c1.177.886%201.842%202.1%201.842%203.381%200%201.282-.665%202.495-1.84%203.381a.9.9%200%201%200%201.083%201.438zm4.183%201.87C21.137%2016.943%2022%2014.554%2022%2012.009c0-2.53-.853-4.907-2.349-6.65l-.017-.021-.024-.028a.9.9%200%201%200-1.36%201.18l.022.025.013.015c1.211%201.413%201.915%203.373%201.915%205.48%200%202.117-.712%204.087-1.935%205.501a.9.9%200%200%200%201.361%201.178zm-15.252%200a.9.9%200%200%200%201.36-1.178C4.513%2016.097%203.8%2014.127%203.8%2012.01c0-2.106.704-4.066%201.915-5.479l.013-.015.022-.025a.9.9%200%200%200-1.36-1.18l-.024.028-.017.02C2.853%207.102%202%209.478%202%2012.01c0%202.545.863%204.934%202.374%206.68z%22%2F%3E%0A%3C%2Fsvg%3E");
+            `);
+            shareIcon.addEventListener('click', (e) => {
+              e.preventDefault();
+              e.stopPropagation();
 
-  htmlLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+              var url = getUrlMp3(audiosToDownload.fullId + '_' + audiosToDownload.track_addon, (data) => {
+                var url = parseMp3(data);
 
-    var url = getUrlMp3(audiosToDownload.fullId + '_' + audiosToDownload.track_addon, (data) => {
-      var url = parseMp3(data);
-
-      if (url) {
-        url.then(function (link) {
-          chrome.runtime.sendMessage({ action: 'playVKAudio', link }, () => {
-
-            const tracks = [ ...document.querySelectorAll(`.${audioClassName}`) ];
-            let active = tracks.findIndex(track => {
-              const trackInfo = JSON.parse(track.getAttribute(dataAttr));
-              return trackInfo.fullId === audiosToDownload.fullId;
-            });
-
-            const listener = () => {
-              if (tracks.length <= active) {
-                chrome.runtime.onMessage.removeListener(listener);
-                return;
-              }
-
-              const audiosToDownload = JSON.parse(tracks[ ++active ].getAttribute(dataAttr));
-
-              getUrlMp3(audiosToDownload.fullId + '_' + audiosToDownload.track_addon, (data) => {
-                const url = parseMp3(data);
                 if (url) {
-                  url.then(link => chrome.runtime.sendMessage({ action: 'playVKAudio.loadNextRes', link, active }), () => {
+                  url.then(function (link) {
+                    chrome.runtime.sendMessage({ action: 'playVKAudio', link }, () => {
+
+                      const tracks = [ ...document.querySelectorAll(`.${audioClassName}`) ];
+                      let active = tracks.findIndex(track => {
+                        const trackInfo = JSON.parse(track.getAttribute(dataAttr));
+                        return trackInfo.fullId === audiosToDownload.fullId;
+                      });
+
+                      const listener = () => {
+                        active += 1;
+                        if (tracks.length <= active) {
+                          chrome.runtime.onMessage.removeListener(listener);
+                          return;
+                        }
+
+                        const audiosToDownload = JSON.parse(tracks[ active ].getAttribute(dataAttr));
+
+                        getUrlMp3(audiosToDownload.fullId + '_' + audiosToDownload.track_addon, (data) => {
+                          const url = parseMp3(data);
+                          if (url) {
+                            url.then(link => chrome.runtime.sendMessage({
+                              action: 'playVKAudio.loadNextRes',
+                              link,
+                              active
+                            }), () => {
+                            });
+                            console.log(`Item with id: ${active} was added`);
+                          } else {
+                            console.warn(`Item with id: ${active} ALARM!!!!`);
+                          }
+
+                          listener();
+                        });
+                      };
+
+                      listener();
+                    });
                   });
-                  console.log(`Item with id: ${active} was added`);
                 } else {
-                  console.warn(`Item with id: ${active} ALARM!!!!`);
-                }
-
-                listener();
-              });
-            };
-
-            listener();
-          });
-        });
-      } else {
-        const error = document.createElement('div');
-        error.innerText = 'Unavailable, ЛОХ, ПИДР!';
-        error.setAttribute('style', `
+                  const error = document.createElement('div');
+                  error.innerText = 'Unavailable, ЛОХ, ПИДР!';
+                  error.setAttribute('style', `
           z-index: 100000; 
           position: fixed; 
           left: 10px;
@@ -136,13 +133,29 @@ function updateOneAudio(audio) {
           padding: 10px 12px;
           font-weight: 600;
         `);
-        document.body.appendChild(error);
-        setTimeout(() => document.body.removeChild(error), 3000);
-        error.addEventListener('click', () => document.body.removeChild(error));
+                  document.body.appendChild(error);
+                  setTimeout(() => document.body.removeChild(error), 3000);
+                  error.addEventListener('click', () => document.body.removeChild(error));
+                }
+              });
+            });
+
+            actions.appendChild(shareIcon);
+
+            const parser = new DOMParser();
+            const baloonDocument = parser.parseFromString('<div class="tt_w tt_black toleft tt_down" style="position: absolute; opacity: 1; top: -27px; left: 18px; pointer-events: auto; display: block;"><div class="tt_text">Stream to Kodi</div></div>', 'text/html')
+            const baloon = baloonDocument.body.firstChild;
+
+            shareIcon.addEventListener('mouseenter', () => actions.appendChild(baloon));
+            shareIcon.addEventListener('mouseleave', () => actions.removeChild(baloon));
+          }
+        }
       }
     });
+  }).observe(audio, {
+    childList: true,
+    subtree: true,
   });
-  audioActs.insertBefore(downloadButton, audioActs.firstChild);
 }
 
 function getAudioInfo(audio) {
